c9abb504c6a05476430588e80d27c57d5f28fd7e
martovski5 <krustinson88@gmail.com>
2022-06-25 20:58:50 +0300

    change index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 module.exports = function reverse (n) {
- const num = n;
- const numToString = String(num);
- let rev = numToString.split('').reverse().join('');
- let strToNum = Number(rev);
-}
-}
+//  const num = n;
+//  if ( Math.sign(num) == -1 ) {
+//     return -num;
+//  } 
+//  const numToString = String(num);
+//  let rev = numToString.split('').reverse().join('');
+//  let strToNum = Number(rev);
+//  return strToNum;
+const num = n;
+      const numToString = String(num);
+      let rev = numToString.split('');
+      console.log(rev);
+      if (rev.includes('-')) {
+        rev.shift();
+      }
+      console.log(rev);
+      let rev2 = rev.reverse().join('');
+      console.log(typeof rev2);
+      let strToNum = Number(rev2);
+      return strToNum;
+      
+};
+
