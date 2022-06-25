9af0289481a79115c5ff06a771a81b1562f399fa
Roman Krustinson <77626025+martovski5@users.noreply.github.com>
2022-06-25 20:12:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+ const num = n;
+ const numToString = String(num);
+ let rev = numToString.split('').reverse().join('');
+ let strToNum = Number(rev);
+}
 }
